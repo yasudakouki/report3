56b98493d4e4967362b557f7713bea73d2fc41b9
Yasuda Kouki <e225703@ie.u-ryukyu.ac.jp>
2022-11-16 01:31:31 +0900

    カプセル化した

--- app/src/main/java/jp/ac/uryukyu/ie/e225703/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225703/Enemy.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e225703;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -21,11 +21,11 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        this.setName(name);
+        this.setHitPoint(maximumHP);
+        this.setAttack(attack);
+        this.setDead(false);
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", getName(), getHitPoint(), getAttack());
     }
 
     /**
@@ -36,7 +36,7 @@ public class Enemy {
     public void attack(Hero hero){
         if (this.dead == false) {
             int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), hero.getName(), damage);
             hero.wounded(damage);
         }
     }
@@ -50,8 +50,17 @@ public class Enemy {
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 
+    public void setName(String _name) { this.name = _name; }
+    public void setHitPoint(int _hitpoint) { this.hitPoint = _hitpoint; }
+    public void setAttack(int _attack) { this.attack = _attack; }
+    public void setDead(boolean _dead) { this.dead = _dead; }
+
+    public String getName() { return this.name; }
+    public int getHitPoint() { return this.hitPoint; }
+    public int getAttack() { return this.attack; }
+    public boolean isDead() { return this.dead; }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e225703/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225703/Hero.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e225703;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -21,11 +21,11 @@ public class Hero {
      * @param attack ヒーローの攻撃力
      */
     public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        this.setName(name);
+        this.setHitPoint(maximumHP);
+        this.setAttack(attack);
+        this.setDead(false);
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", getName(), getHitPoint(), getAttack());
     }
 
     /**
@@ -35,7 +35,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), e.getName(), damage);
         e.wounded(damage);
     }
 
@@ -48,7 +48,17 @@ public class Hero {
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
+
+    public void setName(String _name) { this.name = _name; }
+    public void setHitPoint(int _hitpoint) { this.hitPoint = _hitpoint; }
+    public void setAttack(int _attack) { this.attack = _attack; }
+    public void setDead(boolean _dead) { this.dead = _dead; }
+
+    public String getName() { return this.name; }
+    public int getHitPoint() { return this.hitPoint; }
+    public int getAttack() { return this.attack; }
+    public boolean isDead() { return this.dead; }
 }
